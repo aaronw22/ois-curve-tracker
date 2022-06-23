b6af0d60c0aeb4f2cb729924c46005157fdc373e
Aaron Wong <aaron.wong22@gmail.com>
2022-06-23 23:55:17 +0100

    rewrite in base R to cut down on dependencies

--- R/job.R
+++ R/job.R
@@ -1,11 +1,6 @@
 # Scrape ASX 30 Day Interbank Cash Rate Futures Implied Yield Target from PDF
 
-library(dplyr) 
-library(tidyr)
-library(purrr)
-library(lubridate)
 library(stringr)
-library(readr)
 library(tesseract)
 library(magick)
 
@@ -24,54 +19,55 @@ download.file(pdf_url, pdf_path, mode = "wb")
 # Read the PDF as an image, crop it, and remove gridlines
 full_image <- image_read_pdf(pdf_path)
 
-table_image <- full_image %>%
-  magick::image_crop(geometry = geometry_area(width = 870 * 3.1,
+table_image <- 
+  magick::image_crop(full_image, geometry = geometry_area(width = 870 * 3.1,
                                               height = 38 * 3.1,
                                               x_off = 168 * 3.1,
-                                              y_off = 650 * 3.1)) %>%
-  image_quantize(colorspace = "gray") %>%
-  image_transparent(color = "white", fuzz = 48)
+                                              y_off = 650 * 3.1))
+table_image <- image_quantize(table_image, colorspace = "gray")
+table_image <- image_transparent(table_image, color = "white", fuzz = 48)
 
 # Extract the characters from the image
-strings <- table_image %>%
-  ocr() %>%
-  str_split(pattern = "\n") %>%
-  unlist()
+strings <- ocr(table_image)
+strings <- str_split(strings, pattern = "\n")
+strings <- unlist(strings)
 
 strings <- strings[strings != ""]
 
-string_list <- map(strings, ~(str_split(.x, " ")[[1]]))
+string_list <- lapply(strings, function(x) unlist(str_split(x, " ")))
 
-# Create a tibble with our newly-scraped data
-new_data <- tibble(date = string_list[[1]],
-                   cash_rate = string_list[[2]],
-                   scrape_date = Sys.Date()) %>%
-  mutate(date = lubridate::my(date))
+new_data <- as.data.frame(string_list, col.names = c("date", "cash_rate"))
+new_data$scrape_date <- Sys.Date()
+new_data$date <- as.Date(paste0("01-", new_data$date), "%d-%b-%y")
 
 # The decimal point is not always picked up; add it in
 # Note we are assuming all future cash rates are <10%
+new_data$cash_rate <-
+  ifelse(
+    str_sub(new_data$cash_rate, 2, 2) == ".",
+    new_data$cash_rate,
+    paste0(
+      str_sub(new_data$cash_rate, 1, 1),
+      ".",
+      str_sub(new_data$cash_rate, 2L, -1L)
+    )
+  )
 
-new_data <- new_data %>%
-  mutate(cash_rate = if_else(str_sub(cash_rate, 2, 2) == ".",
-                             cash_rate,
-                             paste0(str_sub(cash_rate, 1, 1),
-                                    ".",
-                                    str_sub(cash_rate, 2L, -1L))),
-         cash_rate = as.numeric(cash_rate))
-
+new_data$cash_rate <- as.numeric(new_data$cash_rate)
 
 # Write a CSV of today's data
-write_csv(new_data, file.path("daily-data",
-                              paste0("scraped_cash_rate_", Sys.Date(), ".csv")))
-write_csv(new_data, file.path("latest-data",
-                              paste0("scraped_cash_rate_latest.csv")))
+write.csv(new_data, file.path("daily-data",
+                              paste0("scraped_cash_rate_", Sys.Date(), ".csv")),
+          row.names = FALSE)
+write.csv(new_data, file.path("latest-data",
+                              paste0("scraped_cash_rate_latest.csv")),
+          row.names = FALSE)
 
 # Load all existing data, combine with latest data
-all_data <- file.path("daily-data") %>%
-  list.files(pattern = ".csv",
-             full.names = TRUE) %>%
-  read_csv(col_types = "DdD")
+all_data <- list.files(file.path("daily-data"), pattern = ".csv", full.names = TRUE)
+all_data <- lapply(all_data, function(x) read.csv(x, colClasses = c("Date", "numeric", "Date")))
+all_data <- do.call(rbind, all_data)
 
-write_csv(all_data,
-        file = file.path("combined-data",
-                         "all_data.csv"))
+write.csv(all_data,
+          file = file.path("combined-data", "all_data.csv"),
+          row.names = FALSE)
