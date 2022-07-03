d1e06795987b6fc4b730fe600ba17784ef7fb7b7
Aaron Wong <aaron.wong@e61.in>
2022-07-04 00:03:18 +0100

    add basic error correction for failed scrapes to ensure continuous time series by LOCF missing values

--- R/job.R
+++ R/job.R
@@ -68,4 +68,13 @@ all_data <- rbindlist(list(all_data, new_data))
 # Remove duplicated scrape dates
 all_data <- unique(all_data, by = c("date", "scrape_date"))
 
+# Automatically fix any gaps in the series from failed scrapes with last obs
+# carried forward. Only do this for the past week to avoid changing historical
+# data where the old series might not extend as far into the future
+all_dates <- seq.Date(from = min(all_data$scrape_date), to = max(all_data$scrape_date), by = "1 day")
+all_dates <- CJ(scrape_date = all_dates, date = all_data[scrape_date == max(scrape_date), date])
+all_data <- all_data[all_dates, on = .(date, scrape_date)]
+all_data <- all_data[!is.na(cash_rate) | scrape_date >= Sys.Date() - 7]
+all_data[, cash_rate := nafill(cash_rate, "locf"), by = .(date)]
+
 fwrite(all_data, file = file.path("combined-data", "all_data.csv"))
