5b029c12182e985c0d16eea96657a8a89f8a9e7e
Aaron Wong <aaron.wong@e61.in>
2023-07-21 12:05:28 +0100

    replace pdf script with dynamic webscraper because the pdf is no longer updated

--- R/job.R
+++ R/job.R
@@ -1,74 +1,45 @@
-# Scrape ASX 30 Day Interbank Cash Rate Futures Implied Yield Target from PDF
+# Scrape ASX 30 Day Interbank Cash Rate Futures Implied Yield Target from website
 
 library(data.table)
-library(stringr)
-library(tesseract)
-library(magick)
+library(rvest)
+library(RSelenium)
 
 # Set the timezone so files save with correct date
 Sys.setenv(TZ = "Australia/Sydney")
 
-# Download PDF from the ASX
-pdf_url <- "https://www.asx.com.au/data/trt/ib_expectation_curve_graph.pdf"
-pdf_path <- tempfile(fileext = ".pdf")
-download.file(pdf_url, pdf_path, mode = "wb")
+rD <- rsDriver(browser = "chrome")
 
-# Note that the table in the PDF appears to be part of an image, rather than a
-# normal PDF table. This means we need to use optical character recognition to
-# extract it
+# Assign the client to an object
+remDr <- rD[["client"]]
 
-# Read the PDF as an image, crop it, and remove gridlines
-full_image <- image_read_pdf(pdf_path)
+remDr$navigate("https://www.asx.com.au/markets/trade-our-derivatives-market/derivatives-market-prices/short-term-derivatives")
+pg_source <- remDr$getPageSource()[[1]]
 
-table_image <- 
-  magick::image_crop(full_image, geometry = geometry_area(
-    width = 880 * 3.1,
-    height = 38 * 3.1,
-    x_off = 170 * 3.1,
-    y_off = 650 * 3.1
-    ))
-table_image <- image_quantize(table_image, colorspace = "gray")
-table_image <- image_transparent(table_image, color = "white", fuzz = 55)
+cr_futures <-
+  read_html(pg_source) |> 
+  html_elements(xpath = "/html/body/div[1]/div/div[3]/div/div[1]/div/div/section/section/div[2]/div/div[4]/div[1]/table") |> 
+  html_table()
 
-# Extract the characters from the image
-strings <- ocr(table_image)
-strings <- str_split(strings, pattern = "\n")
-strings <- unlist(strings)
+cr_futures <- setDT(cr_futures[[1]])
 
-strings <- strings[strings != ""]
+setnames(cr_futures, c("Expiry Date", "Previous Settlement", "Previous Settlement Time"), c("date", "cash_rate", "scrape_date"))
 
-string_list <- lapply(strings, function(x) unlist(str_split(x, " ")))
+cr_futures <- cr_futures[, .(date, cash_rate, scrape_date)]
 
-print(string_list)
-
-# Convert to dates, removing the ocassional invalid date that appears
-string_list[[1]] <- as.IDate(paste0("01-", string_list[[1]]), "%d-%b-%y")
-string_list[[1]] <- string_list[[1]][!is.na(string_list[[1]])]
-
-new_data <- as.data.frame(string_list, col.names = c("date", "cash_rate"))
-setDT(new_data)[, scrape_date := as.IDate(Sys.Date())]
-
-# The decimal point is not always picked up; add it in
-# Note we are assuming all future cash rates are <10%
-new_data[, cash_rate := ifelse(
-  str_sub(cash_rate, 2, 2) == ".",
-  cash_rate,
-  paste0(
-    str_sub(cash_rate, 1, 1),
-    ".",
-    str_sub(cash_rate, 2L, -1L)
-  )
-)]
-
-new_data[, cash_rate := as.numeric(cash_rate)]
+cr_futures[, cash_rate := gsub("^(.*)As of \\d+/\\d+/\\d+", "\\1", cash_rate)]
+cr_futures[, cash_rate := trimws(cash_rate)]
+cr_futures[, scrape_date := gsub("As of (\\d+/\\d+/\\d+)", "\\1", scrape_date)]
+cr_futures[, date := as.IDate(paste0("01 ", date), format = "%d %b %y")]
+cr_futures[, scrape_date := as.IDate(scrape_date, format = "%d/%m/%y")]
+cr_futures[, cash_rate := 100 - as.numeric(cash_rate)]
 
 # Write a CSV of today's data
-fwrite(new_data, file.path("daily-data", paste0("scraped_cash_rate_", Sys.Date(), ".csv")))
-fwrite(new_data, file.path("latest-data", paste0("scraped_cash_rate_latest.csv")))
+fwrite(cr_futures, file.path("daily-data", paste0("scraped_cash_rate_", Sys.Date(), ".csv")))
+fwrite(cr_futures, file.path("latest-data", paste0("scraped_cash_rate_latest.csv")))
 
 # Load existing data, combine with latest data
 all_data <- fread(file.path("combined-data", "all_data.csv"))
-all_data <- rbindlist(list(all_data, new_data))
+all_data <- rbindlist(list(all_data, cr_futures))
 
 # Remove duplicated scrape dates
 all_data <- unique(all_data, by = c("date", "scrape_date"))
