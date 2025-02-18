840157f7695f87f48f46aa4b85b3e56bde868ab6
Aaron Wong <aaron.wong@e61.in>
2025-02-18 14:02:52 +1100

    fix scraper deleting old data in all_data file

--- R/job.R
+++ R/job.R
@@ -23,7 +23,7 @@ all_data <- unique(all_data, by = c("date", "scrape_date"))
 # carried forward. Only do this for the past week to avoid changing historical
 # data where the old series might not extend as far into the future
 all_dates <- seq.Date(from = min(all_data$scrape_date), to = max(all_data$scrape_date), by = "1 day")
-all_dates <- CJ(scrape_date = all_dates, date = all_data[scrape_date == max(scrape_date), date])
+all_dates <- CJ(scrape_date = all_dates, date = unique(all_data$date))
 all_data <- all_data[all_dates, on = .(date, scrape_date)]
 all_data <- all_data[!is.na(cash_rate) | scrape_date >= Sys.Date() - 7]
 all_data[, cash_rate := nafill(cash_rate, "locf"), by = .(date)]
